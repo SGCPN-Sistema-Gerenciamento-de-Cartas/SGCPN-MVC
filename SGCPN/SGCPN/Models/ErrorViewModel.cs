b3bc89e401cc2ede7414d5e94240b88017159606
Ariel Lopes <ariellopes888@gmail.com>
2023-01-16 17:00:59 -0300

    Iniciando projeto SGCPN MVC

---
+++ SGCPN/SGCPN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SGCPN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
