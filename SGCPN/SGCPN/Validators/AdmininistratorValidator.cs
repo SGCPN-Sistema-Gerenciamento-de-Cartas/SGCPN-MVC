5d29ddbd98868ccf8f757c1e2a69acfb029f6736
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2023-01-25 22:25:06 -0300

    Arrumando as validações de administrador

--- SGCPN/SGCPN/Validators/AdmininistratorValidator.cs
+++ SGCPN/SGCPN/Validators/AdmininistratorValidator.cs
@@ -9,158 +9,135 @@ namespace SGCPN.AdministratorValidation
     public class AdmininistratorValidator : AbstractValidator<Administrator>
     {
 
-        public AdmininistratorValidator(string pattern)
+        public AdmininistratorValidator()
         {
             RuleFor(x => x.Name)
-                .NotEmpty()
 
+                .NotEmpty()
                 .WithMessage("O nome não pode ser vazio")
 
                 .NotNull()
-
                 .WithMessage("O nome não pode ser nulo")
 
                 .MinimumLength(3)
-
                 .WithMessage("O nome deve conter no mínimo 3 caracteres")
 
                 .MaximumLength(100)
-
                 .WithMessage("O nome deve conter no máximo 100 caracteres");
 
             RuleFor(x => x.Email)
 
                 .NotEmpty()
-
                 .WithMessage("O email não pode ser vazio")
 
                 .NotNull()
-
                 .WithMessage("O email não pode ser nulo")
 
                 .EmailAddress()
-
                 .WithMessage("Formato de email inválido");
 
             RuleFor(x => x.Password)
 
                 .NotEmpty()
-
                 .WithMessage("A senha não pode ser vazia")
 
                 .NotNull()
-
                 .WithMessage("A senha não pode ser nula")
 
-                .Must(pass => Regex.IsMatch(pass, @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"))
-                .WithMessage("Formato de senha inválido");
+                .MinimumLength(5)
+                .WithMessage("A senha deve conter no mínimo 5 caracteres")
+
+                .MaximumLength(20)
+                .WithMessage("A senha deve conter no máximo 20 caracteres");
 
             RuleFor(x => x.Celullar)
-                .NotEmpty()
 
+                .NotEmpty()
                 .WithMessage("O celular não pode ser vazio")
 
-                .Must(cel => Regex.IsMatch(cel, @"^\([1 - 9]{ 2}\) (?:[2 - 8] | 9[1 - 9])[0 - 9]{ 3}\-[0 - 9]{ 4}$"))
+                .MinimumLength(12)
+                .WithMessage("O celular deve conter no mínimo 12 caracteres")
 
-                .WithMessage("Formato de celular inválido");
+                .MaximumLength(20)
+                .WithMessage("O celular deve conter no máximo 20 caracteres");
 
             RuleFor(x => x.Cep)
-                .NotEmpty()
 
+                .NotEmpty()
                 .WithMessage("O cep não pode ser vazio")
 
-                .Must(cep => Regex.IsMatch(cep, @"\^([\d]{ 2})\.?([\d]{ 3})\-? ([\d]{ 3})/"))
+                .MinimumLength(10)
+                .WithMessage("O cep deve conter no mínimo 10 caracteres")
 
-                .WithMessage("Formato de cep inválido");
-
+                .MaximumLength(20)
+                .WithMessage("O cep deve conter no máximo 20 caracteres"); 
 
             RuleFor(x => x.Cnpj)
 
-              .NotEmpty()
+                .NotEmpty()
+                .WithMessage("O CNPJ não pode ser vazio")
 
-              .WithMessage("O CNPJ não pode ser vazio")
+                .MinimumLength(14)
+                .WithMessage("O CNPJ deve conter no mínimo 14 caracteres")
 
-              .Must(cnpj => Regex.IsMatch(cnpj, @"(^\d{ 2}\.\d{ 3}\.\d{ 3}\/\d{ 4}\-\d{ 2}$)"))
-
-              .WithMessage("Formato de cnpj inválido");
-
+                .MaximumLength(14)
+                .WithMessage("O CNPJ deve conter no máximo 14 caracteres");
 
             RuleFor(x => x.County)
 
                 .NotEmpty()
-
                 .WithMessage("O município não pode ser vazio")
 
                 .NotNull()
-
                 .WithMessage("O município não pode ser nulo");
-
 
             RuleFor(x => x.Address)
 
                 .NotEmpty()
-
                 .WithMessage("O endereço não pode ser vazio")
 
                 .NotNull()
-
                 .WithMessage("O endereço não pode ser nulo");
-
 
             RuleFor(x => x.State)
 
                 .NotEmpty()
-
                 .WithMessage("O estado não pode ser vazio")
 
                 .NotNull()
-
                 .WithMessage("O estado não pode ser nulo");
-
 
             RuleFor(x => x.Sex)
 
                 .NotEmpty()
-
                 .WithMessage("O campo não pode ser vazio")
 
                 .NotNull()
-
                 .WithMessage("O campo não pode ser nulo");
 
-
             RuleFor(x => x.Telephone)
-               .NotEmpty()
 
+               .NotEmpty()
                .WithMessage("O telefone não pode ser vazio")
 
-               .Must(tel => Regex.IsMatch(tel, @"^\([0-9]{2}?(\s|-)?(9?[0-9]{4})-?([0-9]{4}$)"))
-
+               .MaximumLength(10)
                .WithMessage("Formato de telefone inválido");
-
 
             RuleFor(x => x.Number)
 
                .NotEmpty()
-
                .WithMessage("O número não pode ser vazio")
 
                .NotNull()
-
                .WithMessage("O número não pode ser nulo");
-
 
             RuleFor(x => x.AddressComplement)
 
                .NotEmpty()
-
                .WithMessage("O complemento não pode ser vazio")
 
                .NotNull()
-
                .WithMessage("O complemento não pode ser nulo");
-
-
 
         }
 
